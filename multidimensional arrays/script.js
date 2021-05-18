47256561967b3d9a66e635cf36f954782e77062e
pain-arch <himulol123@gmail.com>
2021-05-18 14:57:09 +0600

    update

---
+++ multidimensional arrays/script.js
@@ -0,0 +1,19 @@
+var data = [
+    ["tasdiq", 20, "Male", "B.Sc"],
+    ["nusu", 19, "Male", "B.Sc"],
+    ["mim", 20, "Male", "B.Sc"],
+    ["rishan", 19, "Male", "B.Sc"],
+    ["orna", 20, "Male", "B.Sc"],
+
+];
+
+document.write("<table border='1px' cellspacing='0px'>");
+for( var a = 0; a < data.length; a++){
+    document.write("<tr>");
+    for( var b = 0; b < data[a].length; b++){
+        document.write("<td>"+ data[a][b] + "</td>");
+    }
+    document.write("</tr>");
+
+}
+document.write("</table>");
